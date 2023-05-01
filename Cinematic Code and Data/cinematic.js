e0fbeaa0c70c655147ce87a1845daab0d74971cc
Dexter Zhang <dejzhang@ucsc.edu>
2023-04-30 22:57:16 -0700

    added sheep part and working on start screen

--- Cinematic Code and Data/cinematic.js
+++ Cinematic Code and Data/cinematic.js
@@ -24,6 +24,18 @@ class Title extends Phaser.Scene
             } //style
         );
 
+        let box = this.textObject = this.add.text(
+            100, //x
+            450,//y
+            "Dedicated to provide positive experiences for the player. Founded in April 2023 at University of Califorina Santa Cruz.", //text
+            {
+                font: "20px Georgia",
+                color: "#6fa5d9",
+            } //style
+        );
+        
+        box.setWordWrapWidth(250); 
+
         this.input.once('pointerdown', function ()
         {
 
@@ -46,29 +58,59 @@ class SceneA extends Phaser.Scene
 
     preload ()
     {
-        this.load.image('face', 'assets/bro.png');
+        this.load.image('shepard', 'assets/bro.png');
+        this.load.image('scene2', 'assets/scene2.jpg');
+        this.load.image('sheep', 'assets/Sheep.png');
+        this.load.audio('baa', ['assets/baa.mp3']);
+        this.load.audio('backgroundMusic', ['assets/SOM.mp3']);
+        
     }
 
     create ()
     {
-        const image = this.add.sprite(400, 300, 'face').setAlpha(1.0);
+        let baa = this.sound.add('baa', { loop: false });
+        let backgroundMusic = this.sound.add('backgroundMusic', { loop: true });
+
+        const img = this.add.sprite(400, 300, 'scene2').setAlpha(1.0);
+        img.scale = 0.2;
+
+        const image = this.add.sprite(300, 300, 'shepard').setAlpha(1.0);
         image.scale = 0.25;
         
         this.textObject = this.add.text(
-            300, //x
-            100,//y
+            50, //x
+            80,//y
             "Where are all my shep?", //text
             {
                 font: "30px Georgia",
-                color: "#6fa5d9",
+                color: "#E53D1B",
             } //style
         );
+
+        backgroundMusic.play();
+        console.log("start background");
+        this.time.delayedCall(1000, () =>{
+            const sheep = this.add.sprite(400, 500, 'sheep')
+            sheep.scale = 0.25;
+            baa.play();
+            this.tweens.add({
+                targets: sheep,
+                x: 700,
+                flipX: false,
+                yoyo: true,
+                duration: 2000,
+                repeat: -1
+            });
+        });
 
         this.input.once('pointerdown', function ()
         {
 
             console.log('From SceneA to SceneB');
 
+            // fade to black
+            this.cameras.main.fadeOut(6000, 0, 0, 0)
+            
             this.scene.start('sceneB');
 
         }, this);
@@ -85,27 +127,76 @@ class SceneB extends Phaser.Scene
     preload ()
     {
         this.load.image('arrow', 'assets/Tepig.png');
+        this.load.image('scene3', 'assets/scene3.jpg');
     }
 
     create ()
     {
-        const img = this.arrow = this.add.sprite(400, 300, 'arrow').setOrigin(0, 0.5);
+        this.cameras.main.fadeIn(6000);
+
+        const img = this.scene3 = this.add.sprite(400, 300, 'scene3');
         img.scale = 0.25;
 
-        this.input.once('pointerdown', function (event)
-        {
+        this.textObject = this.add.text(
+            100, //x
+            100,//y
+            "My 99 Shep", //text
+            {
+                font: "50px Georgia",
+                color: "#E5CBC6",
+            } //style
+        );
+        
+        const rect = new Phaser.Geom.Rectangle(250, 200, 300, 200);
+        const graphics = this.add.graphics({ fillStyle: { color: 0x0000ff } });
+        graphics.strokeRectShape(rect);
 
-            console.log('From SceneB to SceneC');
+        // graphics.fillRectShape(rect);
+        let pos = 250;
+        let space = 50;
+        this.textObject = this.add.text(
+            100, //x
+            pos,//y
+            "Play", //text
+            {
+                font: "20px Georgia",
+                color: "#6fa5d9",
+            } //style
+        );
+        this.textObject = this.add.text(
+            100, //x
+            pos+space,//y
+            "Options", //text
+            {
+                font: "20px Georgia",
+                color: "#6fa5d9",
+            } //style
+        );
+        this.textObject = this.add.text(
+            100, //x
+            pos+2*space,//y
+            "Credits", //text
+            {
+                font: "20px Georgia",
+                color: "#6fa5d9",
+            } //style
+        );
+        this.textObject = this.add.text(
+            100, //x
+            pos+3*space,//y
+            "Quit", //text
+            {
+                font: "20px Georgia",
+                color: "#6fa5d9",
+            } //style
+        );
 
-            this.scene.start('sceneC');
-
-        }, this);
     }
 
-    update (time, delta)
-    {
-        this.arrow.rotation += 0.01;
-    }
+    // update (time, delta)
+    // {
+    //     this.arrow.rotation += 0.01;
+    // }
 }
 
 class SceneC extends Phaser.Scene
@@ -166,17 +257,15 @@ class SceneD extends Phaser.Scene {
             repeat: 0,
             duration: 3000
         });
-        this.input.keyboard.once('keydown-SPACE', () => {
-            // fade to black
-            this.cameras.main.fadeOut(6000, 0, 0, 0)
-
-            // this.cameras.main.once('camerafadeincomplete', function (camera) {
-            //     camera.fadeOut(6000);
-            // });
-        })
     
         this.cameras.main.fadeIn(6000);
         console.log('SceneD');
+        // this.time.delay()
+        this.input.once('pointerdown', () => {
+            // fade to black
+            this.cameras.main.fadeOut(6000, 0, 0, 0)
+            console.log('fade to black');
+        })
     }
     update(){}
 }
